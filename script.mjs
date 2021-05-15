0e34d2db16e767943d03aab8689ccb1d1e562236
宮川優斗 <miyakaway7@gmail.com>
2021-05-15 18:28:18 +0900

    シバン間違ってたので直して半角スペース入れた

--- script.mjs
+++ script.mjs
@@ -1,7 +1,7 @@
-#! /ur/bin/env zx
+#! /usr/bin/env zx
 console.log('Hellow zx');
 const count = parseInt(await $`ls -1 | wc -l`);
 console.log(`Files count: ${count}`);
 
-const name = await question('What is your username');
+const name = await question('What is your username ');
 console.log(name);
