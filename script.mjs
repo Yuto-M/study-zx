e1e491ce719b0b0f20216182a6acc3716161b44f
宮川優斗 <miyakaway7@gmail.com>
2021-05-15 18:24:15 +0900

    questionで対話的にも書ける

--- script.mjs
+++ script.mjs
@@ -2,3 +2,6 @@
 console.log('Hellow zx');
 const count = parseInt(await $`ls -1 | wc -l`);
 console.log(`Files count: ${count}`);
+
+const name = await question('What is your username');
+console.log(name);
