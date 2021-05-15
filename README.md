b1b100f60e76d87ae00e08ee8540df2effbc260d
宮川優斗 <miyakaway7@gmail.com>
2021-05-15 18:49:14 +0900

    ファイルのread動作試した

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # study-zx
-zxのお試し用repo
+[zx](https://github.com/google/zx)のお試し用repo
+
 
 ## 参考
 https://dev.classmethod.jp/articles/shell-zx/
--- script.mjs
+++ script.mjs
@@ -3,5 +3,15 @@ console.log('Hellow zx');
 const count = parseInt(await $`ls -1 | wc -l`);
 console.log(`Files count: ${count}`);
 
-const name = await question('What is your username ');
-console.log(name);
+console.log(chalk.blue('Hello world!'))
+
+
+const content = await fs.readFile('./test-assets/src-list');
+console.log(`content is \n${content}`);
+
+
+
+
+// 対話的に処理する時はquestion()を使う
+// const name = await question('What is your username ');
+// console.log(name);
